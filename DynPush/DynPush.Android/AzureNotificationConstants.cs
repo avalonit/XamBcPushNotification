0c667fe5cf76eceae40aeaf48ce216d699d41924
alberto <alberto.valenti@varprime.com>
2020-04-17 14:42:48 +0200

    DynPush.Android

---
+++ DynPush/DynPush.Android/AzureNotificationConstants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DynPush.Droid
+{
+    public class AzureNotificationConstants
+    {
+        // Azure app-specific connection string and hub path
+        public const string ListenConnectionString = "Endpoint=sb://{your_hub_namespace}.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey={your_hub_key}";
+        public const string NotificationHubName = "{your_hub_name}";
+
+        public const string TAG = "MainActivity";
+        internal static readonly string CHANNEL_ID = "my_notification_channel";
+    }
+}
--- DynPush/DynPush.Android/MainActivity.cs
+++ DynPush/DynPush.Android/MainActivity.cs
@@ -6,6 +6,8 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Android.Gms.Common;
+using Android.Util;
 
 namespace DynPush.Droid
 {
@@ -19,6 +21,22 @@ namespace DynPush.Droid
 
             base.OnCreate(savedInstanceState);
 
+            if (Intent.Extras != null)
+            {
+                foreach (var key in Intent.Extras.KeySet())
+                {
+                    if (key != null)
+                    {
+                        var value = Intent.Extras.GetString(key);
+                        Log.Debug(AzureNotificationConstants.TAG, "Key: {0} Value: {1}", key, value);
+                    }
+                }
+            }
+
+            IsPlayServicesAvailable();
+            CreateNotificationChannel();
+
+
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
@@ -28,6 +46,46 @@ namespace DynPush.Droid
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        public bool IsPlayServicesAvailable()
+        {
+            int resultCode = GoogleApiAvailability.Instance.IsGooglePlayServicesAvailable(this);
+            if (resultCode != ConnectionResult.Success)
+            {
+                if (GoogleApiAvailability.Instance.IsUserResolvableError(resultCode))
+                    Log.Debug(AzureNotificationConstants.TAG, GoogleApiAvailability.Instance.GetErrorString(resultCode));
+                else
+                {
+                    Log.Debug(AzureNotificationConstants.TAG, "This device is not supported");
+                    Finish();
+                }
+                return false;
+            }
+
+            Log.Debug(AzureNotificationConstants.TAG, "Google Play Services is available.");
+            return true;
+        }
+
+        private void CreateNotificationChannel()
+        {
+            if (Build.VERSION.SdkInt < BuildVersionCodes.O)
+            {
+                // Notification channels are new in API 26 (and not a part of the
+                // support library). There is no need to create a notification
+                // channel on older versions of Android.
+                return;
+            }
+
+            var channelName = AzureNotificationConstants.CHANNEL_ID;
+            var channelDescription = string.Empty;
+            var channel = new NotificationChannel(AzureNotificationConstants.CHANNEL_ID, channelName, NotificationImportance.Default)
+            {
+                Description = channelDescription
+            };
+
+            var notificationManager = (NotificationManager)GetSystemService(NotificationService);
+            notificationManager.CreateNotificationChannel(channel);
         }
     }
 }
